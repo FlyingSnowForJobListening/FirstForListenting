a2750e986bcd23d0280a2d7c7c13fec54ff29ab4
Andy Zhang <andy.zhang0216@outlook.com>
2015-09-09 17:20:39 +0800

    9.9 feature
    service add

---
+++ FS.Platform.Service/ReceiptInstaller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace FS.Platform.Service
+{
+    [RunInstaller(true)]
+    public partial class ReceiptInstaller : System.Configuration.Install.Installer
+    {
+        public ReceiptInstaller()
+        {
+            InitializeComponent();
+            ServiceProcessInstaller process = new ServiceProcessInstaller();
+            process.Account = ServiceAccount.LocalSystem;
+
+            ServiceInstaller serviceAdmin = new ServiceInstaller();
+
+            serviceAdmin.StartType = ServiceStartMode.Automatic;
+            serviceAdmin.ServiceName = "ReceiptPlatForm";
+            serviceAdmin.DisplayName = "Platform of Receipt";
+
+            Installers.Add(process);
+            Installers.Add(serviceAdmin);
+
+        }
+    }
+}
---
+++ FS.Platform.Service/ReceiptSercice.cs
@@ -0,0 +1,37 @@
+﻿using FS.Configuration;
+using FS.Message.Receiption;
+using FS.Platform.Server;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FS.Platform.Service
+{
+    public partial class ReceiptSercice : ServiceBase
+    {
+        public ReceiptSercice()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            FileWatcher.WatcherStart(ConfigurationInfo.PathReceipt, 1, "*.xml");
+            ServiceHost message = new ServiceHost(typeof(MessageService));
+            message.Open();
+            base.OnStart(args);
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+        }
+    }
+}
